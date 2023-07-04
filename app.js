95029075ab3d0d40cf8a032c2c50fb5a0b03ec95
Lee Pierson <awesomehenge@gmail.com>
2023-07-04 11:56:26 +0100

    update

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ const hours = ["5pm-7pm", "7pm-9pm", "9pm-11pm"]
 let chairs = 0;
 let maxChairs = 5;
 
+const bookDnD = document.querySelector("form")
+console.log("booking")
+
+const table = document.getElementById("forecast-table");
+
 const cells = table.getElementsByTagName("td");
+console.log("td")
+
+for (const cell of cells) {
+    const status = cell.getAttribute("data-status");
+    if (status === "open") {
+    
+    }
+  }
+
 
 for ( i=0; i  < chairs; i++){
-console.log("charirs")
+console.log("chairs")
 }
+
+if (chairs = maxChairs){
+    console.log("full")
+    alert("Sorry, we're fully booked.")
+}
+
+Array.from("td")
+
