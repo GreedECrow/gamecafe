503849a7e709257786ae69abaebdf55b9b58a277
Lee Pierson <awesomehenge@gmail.com>
2023-07-06 10:10:57 +0100

    13

--- app.js
+++ app.js
@@ -1,81 +1,114 @@
-// "use strict"
-// console.log("Boom Stick!!")
+"use strict"
+console.log("first")
 
-// const hours = ["5pm-7pm", "7pm-9pm", "9pm-11pm"]
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const gameSelect = document.getElementById("game");
 
-// let chairs = 0;
-// let maxChairs = 5;
+populateUI();
+let ticketPrice = +gameSelect.value;
+console.log(typeof ticketPrice);
 
-// const bookDnD = document.querySelector("form")
-// console.log()
+function setGameData(gameIndex, gamePrice) {
+    localStorage.setItem("selectedGameIndex", gameIndex);
+    localStorage.setItem("selectedGamePrice", gamePrice);
+}
 
-// const table = document.getElementById("ddtable1");
+function updateSeletedCount() {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
-// const cells = document.getElementsByTagName('td');
-// console.log(cells)
+    const seatsIndex = [...selectedSeats].map(function (seat) {
+        return [...seats].indexOf(seat);
+    });
 
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
-// get user intput on the radio buttons 
-// so we know what time they've selected 
+    const selectedSeatsCount = selectedSeats.length;
 
-// get element by id th -
-// if th innerText === userInput 
-//
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
 
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
 
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("selected");
+            }
+        });
+    }
+    const selectedGameIndex = localStorage.getItem("selectedGameIndex");
 
+    if (selectedGameIndex != null) {
+        gameSelect.selectedIndex = selectedGameIndex;
+    }
+}
 
-// for (const cell of cells) {
-//     const status = cell.getAttribute("data-status");
-//     if (status === "open") {
-
-//     }
-//   }
-
-
-// for ( i=0; i  < chairs; i++){
-// console.log("chairs")
-// }
-
-// if (chairs = maxChairs){
-//     console.log("full")
-//     alert("Sorry, we're fully booked.")
-// }
-
-// Array.from("tr")
-
-
-// --------------------------------------------
-
-let inputName;
-
-const confirmButton = document.getElementById('confirmButton')
-
-confirmButton.addEventListener('click', function (e) {
-    e.preventDefault()
-    inputName = document.getElementById('characterName').value
-    console.log(inputName)
-    bookingSystem(inputName)
-
+gameSelect.addEventListener("change", (e) => {
+    ticketPrice = +e.target.value;
+    setGameData(e.target.selectedIndex, e.target.value);
+    updateSeletedCount();
 })
 
 
-
-
-
-
-const cells = document.getElementsByTagName('td');
-// allow user to book time 
-function bookingSystem(userInput) {
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener('click', function () {
-            if (cells[i].classList.contains('occupied')) {
-                alert('this seat has been taken!')
-                console.log('how is this happening!?!')
-            } else {
-                cells[i].innerText = userInput
-                cells[i].classList.add('occupied')
-            }
-        })
+container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
+        e.target.classList.toggle("selected");
     }
+
+    updateSeletedCount();
+});
+
+updateSeletedCount();
+
+
+
+
+
+
+
+
+function CharacterSheet(name, role, job, imageUrl) {
+    this.name = name;
+    this.role = role;
+    this.job = job;
+    this.imageUrl = imageUrl;
+    this.render();
 }
+
+CharacterSheet.prototype.render = function () {
+    const containerElement = document.getElementById("characters");
+
+    const article = document.createElement("article");
+    containerElement.appendChild(article);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = this.name;
+    article.appendChild(h3);
+
+    const p = document.createElement("p");
+    p.textContent = this.role;
+    article.appendChild(p);
+
+    const para = document.createElement("p");
+    para.textContent = this.job;
+    article.appendChild(para);
+
+    const image = document.createElement("img");
+    image.setAttribute("src", this.imageUrl);
+    image.setAttribute("alt", `${this.name} profile shot.`);
+    article.appendChild(image);
+
+}
+
+const ladyviolent = new CharacterSheet("Lady Violent", "Physical DPS", "Punch a dragon in the face.", "assets/monk.jpeg");
+
+const bobertsonmire = new CharacterSheet("Bobertsonmire", "Ranged DPS", "Black Mage.", "assets/blackmage.jpg");
+
+const tankgirl = new CharacterSheet("Tank Girl", "Tank", "Damage Sponge.", "assets/tank.jpg");
+
+const cokezero = new CharacterSheet("Coke Zero", "Healer", "Rubs it better.", "assets/heals.jpg");
